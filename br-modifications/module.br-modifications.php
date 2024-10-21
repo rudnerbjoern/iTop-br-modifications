5d9fe2c23e8bc427e04f307f12a4f797ae5808d2
Bjoern Rudner <rudnerbjoern@hotmail.com>
2024-10-21 15:09:40 +0200

    update dependencies

--- br-modifications/module.br-modifications.php
+++ br-modifications/module.br-modifications.php
@@ -3,14 +3,14 @@
 /**
  * @copyright   Copyright (C) 2022-2024 Björn Rudner
  * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
- * @version     2024-09-11
+ * @version     2024-10-21
  *
  * iTop module definition file
  */
 
 SetupWebPage::AddModule(
     __FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-    'br-modifications/3.1.3',
+    'br-modifications/3.1.4',
     array(
         // Identification
         //
@@ -20,13 +20,28 @@ SetupWebPage::AddModule(
         // Setup
         //
         'dependencies' => array(
+            // add iTop Extensions (install during setup)
             'itop-config-mgmt/3.1.0',
-            'teemip-network-mgmt/3.0.0',
+            'itop-datacenter-mgmt/3.1.0',
+            'itop-storage-mgmt/3.1.0',
+            'itop-virtualization-mgmt/3.1.0',
+            'itop-endusers-devices/3.1.0',
+            'itop-tickets/3.1.0',
+            // add TeemIP Extensions (install from iTop Hub)
+            'teemip-network-mgmt/3.0.1',
+            'teemip-network-mgmt-extended/3.0.1',
             'teemip-datacenter-mgmt-adaptor/3.0.0',
             'teemip-virtualization-mgmt-adaptor/3.0.0',
             'teemip-storage-mgmt-adaptor/3.0.0',
             'teemip-config-mgmt-adaptor/3.0.0',
+            // add my own extensions here (clone from GitHub)
+            'br-location-extension/3.1.9',
+            'br-org-extension/3.1.4',
+            'br-landscape/3.1.2',
+            'br-lifecycle/3.1.4',
+            'br-cpu-extension/3.1.3',
         ),
+
         'mandatory' => false,
         'visible' => true,
 
