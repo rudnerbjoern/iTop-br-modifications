5fbb67df35d83a13744cc11ffa2f2ed432433c2b
Björn Rudner <rudnerbjoern@hotmail.com>
2025-07-22 09:56:40 +0200

    add role fields to lnkContactToFunctionalCI class in datamodel

--- br-modifications/dictionaries/de.dict.br-modifications.php
+++ br-modifications/dictionaries/de.dict.br-modifications.php
@@ -1,9 +1,9 @@
 <?php
 
 /**
- * @copyright   Copyright (C) 2024 Björn Rudner
+ * @copyright   Copyright (C) 2024-2025 Björn Rudner
  * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
- * @version     2024-09-11
+ * @version     2025-07-22
  *
  * Localized data
  */
@@ -19,4 +19,9 @@ Dict::Add('DE DE', 'German', 'Deutsch', array(
     'Class:PhysicalInterface/Name' => '%2$s - %1$s',
     'Class:ServerVirtualInterface/Name' => '%2$s - %1$s',
     'Class:WebServer/Name' => '%2$s - %1$s',
+
+    'Class:lnkContactToFunctionalCI/Attribute:role_id' => 'Rolle',
+    'Class:lnkContactToFunctionalCI/Attribute:role_id+' => 'Eine Rolle aus einer Typologie möglicher Rollen',
+    'Class:lnkContactToFunctionalCI/Attribute:role_name' => 'Rollenname',
+    'Class:lnkContactToFunctionalCI/Attribute:role_name+' => '',
 ));
